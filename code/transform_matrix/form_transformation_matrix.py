0f8ef880b5672b6a2d5627d4bd2fc7f0e5ed767c
Cameron PH Chen <cameron.ph.chen@gmail.com>
2015-10-03 10:33:20 -0400

    clean up algos

--- code/transform_matrix/form_transformation_matrix.py
+++ code/transform_matrix/form_transformation_matrix.py
@@ -7,22 +7,16 @@ def transform(args, workspace_lh, workspace_rh, nsubjs):
   transform_lh = np.zeros((args.nvoxel,args.nfeature,nsubjs))
   transform_rh = np.zeros((args.nvoxel,args.nfeature,nsubjs))
 
-  if args.align_algo in ['ha','ha_syn','ha_syn_kernel','ha_syn_noagg','ha_noagg']:
+  if args.align_algo in ['ha','srm_noneprob']:
     transform_lh = workspace_lh['R']
     transform_rh = workspace_rh['R']
-  elif args.align_algo in ['pha_em','spha_vi', 'spha_vi_tm', 'spha_vi_decomp', 'ppca',  'pica', 'ppca2',  'pica2','ha_syn_l1']:
+  elif args.align_algo in ['srm', 'pca',  'ica']:
     bW_lh = workspace_lh['bW']
     bW_rh = workspace_rh['bW']
     for m in range(nsubjs):
       transform_lh[:,:,m] = bW_lh[m*args.nvoxel:(m+1)*args.nvoxel,:]
       transform_rh[:,:,m] = bW_rh[m*args.nvoxel:(m+1)*args.nvoxel,:]
-  elif args.align_algo in ['ppca_hor','pica_hor']:
-    bW_lh = workspace_lh['R']
-    bW_rh = workspace_rh['R']
-    for m in range(nsubjs):
-      transform_lh[:,:,m] = bW_lh
-      transform_rh[:,:,m] = bW_rh
-  elif args.align_algo in ['ha_sm_retraction','ha_sm_newton']:
+  elif args.align_algo in ['ha_sm_retraction']:
     bW_lh = workspace_lh['W']
     bW_rh = workspace_rh['W']
     for m in range(nsubjs):
