84ae47918b84b00e9b8e89c04af9b9b199c6ecb2
Cameron PH Chen <cameron.ph.chen@gmail.com>
2015-11-03 15:16:21 -0500

    update file name to get roi masks

--- code/preprocessing/forrest/get_template.sh
+++ code/preprocessing/forrest/get_template.sh
@@ -1,4 +1,5 @@
 foldername="forrest_gump/templates/grpbold7Tad/from_mni/"
-filename="MNI152_T1_1mm.nii.gz"
-wget -r http://psydata.ovgu.de/$foldername/$filename 
+wget -r http://psydata.ovgu.de/$foldername/MNI152_T1_1mm.nii.gz
+wget -r http://psydata.ovgu.de/$foldername/HarvardOxford-cortl-maxprob-thr25.nii.gz
+
 
